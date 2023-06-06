6d1b357b374073aefb867b638a3a5c6ffa6f2d2d
Tom Warner <tommwarner@gmail.com>
2023-06-06 17:02:51 -0400

    completed project

--- acme_people_places_things/api/db.js
+++ acme_people_places_things/api/db.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const {STRING, INTEGER} = Sequelize;
-const db = new Sequelize(`postgres://localhost:5432/acme_people_place_things`);
+const db = new Sequelize(`postgres://localhost:5432/acme_people_place_things`,
+    {logging: false});
 
 const Person = db.define("person", {
     name: {
